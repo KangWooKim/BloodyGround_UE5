9bcd2141d9e432c505c2584018084ebbc0ec2f83
Kim KangWoo <85830445+KangWooKim@users.noreply.github.com>
2024-05-24 06:26:23 +0900

    Add files via upload

--- Source/BloodyGround/BloodyGround.Build.cs
+++ Source/BloodyGround/BloodyGround.Build.cs
@@ -8,7 +8,7 @@ public class BloodyGround : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GeometryCollectionEngine", "Niagara", "UMG", "AIModule", "NavigationSystem" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GeometryCollectionEngine", "Niagara", "UMG", "AIModule", "NavigationSystem", "OnlineSubsystem", "OnlineSubsystemSteam" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
